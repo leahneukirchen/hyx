e47838b15c3143bb5d79504c4a8606f49bd39282
Leah Neukirchen <leah@vuxu.org>
2020-12-25 15:38:07 +0100

    use <fcntl.h>, not <sys/fcntl.h>

--- blob.c
+++ blob.c
@@ -4,10 +4,10 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <sys/fcntl.h>
 #include <sys/mman.h>
 
 void blob_init(struct blob *blob)
